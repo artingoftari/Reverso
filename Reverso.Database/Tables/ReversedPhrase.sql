9ec7783c7e44c9d9f56ec9303c7edcb0a17c7892
Artin Goftari <artin.goftari@avanade.com>
2022-08-23 13:56:01 +0200

    Added DB project and table script

---
+++ Reverso.Database/Tables/ReversedPhrase.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ReversedPhrase]
+(
+	[Id]				INT				NOT NULL IDENTITY(1,1),
+	[OriginalPhrase]	nvarchar(MAX)	NOT NULL,
+	[ReversedPhrase]	nvarchar(MAX)	NOT NULL,
+	[Reversed]			datetime		NOT NULL
+)
